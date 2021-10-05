81a1abda0a425d4c728d6d8a96cb28562ef2fd9c
Neeraj Pratap Hazarika <neerajprataphazarika@gmail.com>
2021-10-05 14:56:35 +0530

    [updated] index.js - added PORT=5000 - added get response

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+const callback = () => {
+    console.log('Server started at http://localhost:{PORT}');
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from GDSC IIEST</h1>');
+});
+
+app.listen(PORT, callback);
